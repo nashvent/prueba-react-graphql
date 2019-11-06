6d3da5bc1d97426318b8b34a552f1c285aeffdb1
nashvent <anibal.ventura@ucsp.edu.pe>
2019-11-06 16:47:36 -0500

    proyecto base

---
+++ components/Pokemon.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
+import { AppContext } from '../App';
+
+const Pokemon = () => {
+    return (
+        <AppContext.Consumer>
+            {
+            ({ repositoryCount, edges, onPress }) =>
+            <TouchableOpacity onPress={onPress}>
+                <View style={styles.mainDetails}>
+                  <Text style={styles.mainText}>Numero de repositorios: {repositoryCount}</Text>
+                  <FlatList
+                    data={edges}
+                    renderItem={({ item }) => (
+                      <Text style={styles.title}>{item.node.name}</Text>
+                    )}
+                    keyExtractor={item => item.node.id}
+                  /> 
+
+                </View>
+            </TouchableOpacity>
+            }
+        </AppContext.Consumer>
+        );
+}
+
+//
+const styles = {
+  mainDetails: {
+    padding: 30,
+    alignItems: 'center'
+  },
+  image: {
+    width: 100,
+    height: 100
+  },
+  mainText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  description: {
+    marginTop: 20
+  },
+  types: {
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  type: {
+    padding: 5,
+    width: 100,
+    alignItems: 'center'
+  },
+  typeText: {
+    color: '#fff',
+  },
+  normal: {
+    backgroundColor: '#8a8a59'
+  },
+  fire: {
+    backgroundColor: '#f08030'
+  },
+  water: {
+    backgroundColor: '#6890f0'
+  },
+  electric: {
+    backgroundColor: '#f8d030'
+  },
+  grass: {
+    backgroundColor: '#78c850'
+  },
+  ice: {
+    backgroundColor: '#98d8d8'
+  },
+  fighting: {
+    backgroundColor: '#c03028'
+  },
+  poison: {
+    backgroundColor: '#a040a0'
+  },
+  ground: {
+    backgroundColor: '#e0c068'
+  },
+  flying: {
+    backgroundColor: '#a890f0'
+  },
+  psychic: {
+    backgroundColor: '#f85888'
+  },
+  bug: {
+    backgroundColor: '#a8b820'
+  },
+  rock: {
+    backgroundColor: '#b8a038'
+  },
+  ghost: {
+    backgroundColor: '#705898'
+  },
+  dragon: {
+    backgroundColor: '#7038f8'
+  },
+  dark: {
+    backgroundColor: '#705848'
+  },
+  steel: {
+    backgroundColor: '#b8b8d0'
+  },
+  fairy: {
+    backgroundColor: '#e898e8'
+  }
+}
+
+export default Pokemon;
